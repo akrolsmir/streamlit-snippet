69512eff40b4d4cb85123eabb0fb76cef7c332c6
Austin Chen <akrolsmir@gmail.com>
2021-01-14 16:52:35 -0500

    Accept multiple files and merge them

--- csv-diff.py
+++ csv-diff.py
@@ -7,23 +7,29 @@ def download_link(object_to_download, download_filename, download_link_text):
 
     return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
+def to_lines_multi(files):
+    lines = []
+    for file in files:
+        lines.extend(to_lines(file))
+    return lines
+
 def to_lines(file):
     if not file:
         return []
     return file.read().decode('utf-8').split('\n')
 
-larger = st.file_uploader('CSV with more')
-larger_lines = to_lines(larger)
-st.write(f"## Larger file has {len(larger_lines)} lines")
+larger = st.file_uploader('CSVs with more (can upload multiple)', accept_multiple_files=True)
+larger_lines = to_lines_multi(larger)
+st.write(f"Larger file has {len(larger_lines)} lines total")
 
 
-smaller = st.file_uploader('CSV with less')
-smaller_lines = set(to_lines(smaller))
-st.write(f"## Smaller file has {len(smaller_lines)} lines")
+smaller = st.file_uploader('CSVs with less', accept_multiple_files=True)
+smaller_lines = set(to_lines_multi(smaller))
+st.write(f"Smaller file has {len(smaller_lines)} lines")
 
 
 diff = [line for line in larger_lines if line not in smaller_lines]
-st.write(f"## Difference has {len(diff)} lines")
+st.write(f"Difference has {len(diff)} lines")
 st.write(diff)
 
 link = download_link('\n'.join(diff), 'difference.csv', 'Download difference.csv')
